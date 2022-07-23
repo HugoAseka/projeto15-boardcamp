4274cff5baa374534618dcea81a8d2545a0c6b5e
HugoAseka <hugomaseka@gmail.com>
2022-07-23 11:22:33 -0300

    using JOIN on getgames

--- backend-boardcamp/src/controllers/gamesController.js
+++ backend-boardcamp/src/controllers/gamesController.js
@@ -2,30 +2,20 @@ import connection from "../dbStrategy/postgres.js";
 import joi from "joi";
 
 export async function getGames(req, res) {
-  const { rows: games } = await connection.query("SELECT * FROM games");
-  const { rows: allCategories } = await connection.query(
-    "SELECT * FROM categories"
-  );
   const name = req.query.name;
 
-  //insert categoryName
-  for (let i = 0; i < games.length; i++) {
-    for (let j = 0; j < allCategories.length; j++) {
-      if (games[i].categoryId === allCategories[j].id) {
-        games[i].categoryName = allCategories[j].name;
-        break;
-      }
-    }
-  }
+  const { rows: games } = await connection.query(
+    `SELECT g.*, c.name as "categoryName" FROM games g JOIN categories c ON g."categoryId" = c.id`
+  );
 
   //filter by queryString
-  if(name){
+  if (name) {
     const filteredByInitial = games.filter((obj) => {
-        for( let i = 0 ; i < name.length ; i++){
-            if(obj.name[i].toLowerCase() !== name[i].toLowerCase()) return false;
-        }
-        return true;
-    } )
+      for (let i = 0; i < name.length; i++) {
+        if (obj.name[i].toLowerCase() !== name[i].toLowerCase()) return false;
+      }
+      return true;
+    });
 
     return res.send(filteredByInitial);
   }
@@ -33,29 +23,25 @@ export async function getGames(req, res) {
   res.send(games);
 }
 
+export async function insertGame(req, res) {
+  const newGame = req.body;
+  const allGames = await connection.query("SELECT * FROM games");
+  const gameSchema = joi.object({
+    name: joi.string().required(),
+    image: joi.string().optional(),
+    stockTotal: joi.number().min(0).required(),
+    categoryId: joi.number().min(0).required(),
+    pricePerDay: joi.number().min(0).required(),
+  });
 
+  const { error } = gameSchema.validate(newGame);
 
-export async function insertGame(req,res){
-    const newGame = req.body;
-    const allGames = await connection.query("SELECT * FROM games");
-    const gameSchema = joi.object({
-        name: joi.string().required(),
-        image: joi.string().optional(),
-        stockTotal: joi.number().min(0).required(),
-        categoryId: joi.number().min(0).required(),
-        pricePerDay: joi.number().min(0).required()
-    });
-
-    const {error} = gameSchema.validate(newGame);
-
-    const checkId = () => {
-        for(let i = 0 ; i < allGames.length ; i++){
-            // if( allGames[i].id ===  )
-        }
+  const checkId = () => {
+    for (let i = 0; i < allGames.length; i++) {
+      // if( allGames[i].id ===  )
     }
-    if(error){
-        return res.sendStatus(400);
-    }
-
-
+  };
+  if (error) {
+    return res.sendStatus(400);
+  }
 }
